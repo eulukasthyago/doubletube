00d932234f834c170c6444b910a378a771684c93
lunote <lunote@lenovo-pc.Lucas>
2023-09-12 19:14:32 -0300

    Iniciando o desenvolvimento, criando componentes de exibição do conteudo

---
+++ src/components/CardHome/index.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { Card, CardBody } from "@nextui-org/react";
+
+export default function CardHome() {
+	return(
+		<Card>
+		<CardBody>
+      <p> Aqui um card</p>
+    </CardBody>
+    </Card>
+	)
+}
